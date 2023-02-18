6d2cb374fde683efe7db07035a3634b93058b231
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-02-18 11:34:06 +0000

    Added spotify icon to App file for future reference

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="Miix-header"></header>
+      <header className="Miix-header">
+        <h1>Miix</h1>
+        <FontAwesomeIcon icon={faSpotify} />
+      </header>
     </div>
   );
 }
