1e1663ea9050578163d6b85ed478c69e70f2dbca
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-02-18 13:27:29 +0000

    Set up user authentication and API call to perform basic artist search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,90 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import axios from "axios";
 
 import "./App.css";
 
 function App() {
+  const clientID = "a9911275aba546e082be4ac4a0704f39";
+  const redirectURI = "http://localhost:3000";
+  const authEndpoint = "https://accounts.spotify.com/authorize";
+  const responseType = "token";
+
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  const searchArtists = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: { Authorization: `Bearer ${token}` },
+      params: { q: searchKey, type: "artist" },
+    });
+
+    console.log(data);
+    setArtists(data.artists.items);
+  };
+
+  const renderArtists = () => {
+    return artists.map((artist) => (
+      <div key={artist.id}>
+        {artist.images.length ? (
+          <img width={"100%"} src={artist.images[0].url} alt="" />
+        ) : (
+          <div>No image</div>
+        )}
+        {artist.name}
+      </div>
+    ));
+  };
+
   return (
     <div className="App">
       <header className="Miix-header">
         <h1>Miix</h1>
         <FontAwesomeIcon icon={faSpotify} />
+        {!token ? (
+          <a
+            href={`${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=${responseType}`}
+          >
+            Spotify login
+          </a>
+        ) : (
+          <button onClick={logout}>Logout</button>
+        )}
+
+        {token ? (
+          <form onSubmit={searchArtists}>
+            <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
+            <button type={"submit"}>Search</button>
+          </form>
+        ) : (
+          <h2>Please login</h2>
+        )}
+        {renderArtists()}
       </header>
     </div>
   );
