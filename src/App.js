0fd471ddbf4de59520c0c5c0bb3a6fa70b135264
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-02-22 21:20:04 +0000

    add removeDuplicateTracks

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import axios from "axios";
+// import Navbar from "./components/Navbar";
 
 import "./App.css";
 
 function App() {
   const clientID = "a9911275aba546e082be4ac4a0704f39";
-  //const redirectURI = "http://localhost:3000";
+  const redirectURI = "http://localhost:3000";
   //Uncomment before deploying
-  const redirectURI = "https://deft-haupia-213070.netlify.app";
+  // const redirectURI = "https://deft-haupia-213070.netlify.app";
   const authEndpoint = "https://accounts.spotify.com/authorize";
   const responseType = "token";
   const scope = "user-top-read";
@@ -65,14 +66,23 @@ function App() {
     setTracks(data.items);
     addSongs(data.items);
   };
-
+  
   const addSongs = (newSongs) => {
     let newSongsId = newSongs.map((song) => song.id);
     let playlist = getPlaylist();
     let newPlaylist = playlist.concat(newSongsId);
     setPlaylist(newPlaylist);
     savePlaylist(newPlaylist);
+    removeDuplicateTracks(playlist);
   };
+
+  // Check for unique values and add to song array
+  const removeDuplicateTracks = (playlist) => {
+    let uniqueTracks = playlist.filter((value, index, array) => array.indexOf(value) === index);
+    // console.log("unique: " + uniqueTracks)
+    setPlaylist(uniqueTracks);
+    return playlist;
+}
 
   function getPlaylist() {
     let playlist = JSON.parse(localStorage.getItem("playlist"));
@@ -112,6 +122,7 @@ function App() {
 
   return (
     <div className="App">
+        {/* <Navbar /> */}
       <header className="Miix-header">
         <h1>Miix</h1>
         <FontAwesomeIcon icon={faSpotify} />
