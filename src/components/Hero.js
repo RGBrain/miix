0b63cdbb54cc7b4e4d2863931264bb64597b8a9e
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-24 19:01:06 +0000

    added video styling to jumbotron

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,11 +5,9 @@ import Video from "./assets/Hero-vid.mp4";
 function Hero() {
   return (
     <div className="jumbotron">
-        <video src={Video} autoPlay loop muted />
+        <video className="video-background" src={Video} autoPlay loop muted> </video>
         <h1 className="display-4">Hello, world!</h1>
         <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-4" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <p className="lead">
             <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </p>
