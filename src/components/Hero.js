3b35ddc214900dac4f3d82db0d3fefb293133aa1
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-24 18:21:13 +0000

    added video to jumbotron

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,11 @@
 import React from "react";
 import './styles/Hero.css';
+import Video from "./assets/Hero-vid.mp4";
 
 function Hero() {
   return (
     <div className="jumbotron">
+        <video src={Video} autoPlay loop muted />
         <h1 className="display-4">Hello, world!</h1>
         <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr className="my-4" />
