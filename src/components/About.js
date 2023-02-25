ad1564f30b403c9ea0e8b30d632b9efa5c29b12d
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-25 17:28:19 +0000

    added styling to accordian and background image to how it works section

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About() {
                 <div className="card">
                     <div className="card-header" id="headingOne">
                     <h5 className="mb-0">
-                        <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <button className="btn btn-link" id="aboutBtn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         How it works
                         </button>
                     </h5>
