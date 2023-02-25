b4124481e3cd9078f1c6b39d5441e1fe3541ada6
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-25 14:01:43 +0000

    added how it works section and basic text

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,32 @@ import './styles/About.css';
 
 function About() {
   return (
+    <section className="container.fluid container-about">
+        <h2 id="anchor-howItWorks">HOW IT WORKS</h2>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 d-none">
+            </div>
+            <div class="col-lg-8 col-md-8 col-sm-12">
+                <div id="accordion">
+                <div class="card">
+                    <div class="card-header" id="headingOne">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        How mixx works
+                        </button>
+                    </h5>
+                    </div>
+                
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div class="card-body">
+                        
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
   )
 }
