72a0125b4d3246b7aae88011599304c665166088
Naomi Winchurch <naomi.winchurch@hotmail.co.uk>
2023-02-25 19:20:14 +0000

    Fixing deployment build

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,27 @@
 import React from "react";
-import './styles/Hero.css';
+import "./styles/Hero.css";
 import Video from "./assets/Hero-vid.mp4";
 
 function Hero() {
   return (
     <div className="jumbotron">
-        <video className="video-background" src={Video} autoPlay loop muted> </video>
-        <h1 className="display-4">Make your mixx</h1>
-        <p className="lead">Playlists for everyone to enjoy</p>
-        <p className="lead">
-            <a className="btn btn-primary btn-lg" id="hero-btn" href="#" role="button">Get started</a>
-        </p>
+      <video className="video-background" src={Video} autoPlay loop muted>
+        {" "}
+      </video>
+      <h1 className="display-4">Make your mixx</h1>
+      <p className="lead">Playlists for everyone to enjoy</p>
+      <p className="lead">
+        <a
+          className="btn btn-primary btn-lg"
+          id="hero-btn"
+          href="get-started"
+          role="button"
+        >
+          Get started
+        </a>
+      </p>
     </div>
-  )
+  );
 }
 
 export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,54 @@
 import React from "react";
-import './styles/Navbar.css';
-import logo from './assets/Miix-black.png';
+import "./styles/Navbar.css";
+import logo from "./assets/Miix-black.png";
 
 function Navigation() {
   return (
     <header>
       <section className="navigation">
-      <nav className="navbar navbar-expand-lg navbar-light">
-          <a className="navbar-brand spacer" href="#">
-          <img
-            src={logo}
-            width="150"
-            className="d-inline-block align-top"
-            alt="Miix logo"
-          />
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <a className="navbar-brand spacer" href="/">
+            <img
+              src={logo}
+              width="150"
+              className="d-inline-block align-top"
+              alt="Miix logo"
+            />
           </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
+            <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-              <a className="nav-link spacer" href="#">How it works</a>
+                <a className="nav-link spacer" href="how-it-works">
+                  How it works
+                </a>
               </li>
               <li className="nav-item">
-              <a className="nav-link spacer" href="#">Login</a>
+                <a className="nav-link spacer" href="login">
+                  Login
+                </a>
               </li>
               <li className="nav-item">
-                  <a className="nav-link spacer" href="#">Player</a>
+                <a className="nav-link spacer" href="player">
+                  Player
+                </a>
               </li>
-          </ul>
+            </ul>
           </div>
-      </nav>
+        </nav>
       </section>
     </header>
-
-
-  )
+  );
 }
 
 export default Navigation;
-
-
