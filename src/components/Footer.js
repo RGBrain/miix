b0c0232ac8e2ad1fe47f9f4afe7ab81422cd7411
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-28 21:40:29 +0000

    added github to footer and padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./styles/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <footer>
-            <p>Designed by The Frontend Freestylers 2023 ©</p>
+            <p>Designed by The Frontend Freestylers 2023  </p>
+            <p><FontAwesomeIcon icon={faGithub} /> <a href="https://github.com/Nwinch1512/14-miix">Nwinch1512/14-miix</a></p>
         </footer>
   );
 }
