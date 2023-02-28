3c8ec25e79c31bb6e6fee97c26bb27473b7911b4
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-28 21:22:02 +0000

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
-const Footer = () => {
-  return <div></div>;
-};
+import React from "react";
+import "./styles/Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+            <p>Designed by The Frontend Freestylers 2023 ©</p>
+        </footer>
+  );
+}
 
 export default Footer;
