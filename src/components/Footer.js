7c2bc6fe3b086a2f2f553821b9326d3b457bda45
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-03-04 16:21:13 +0000

    add GitHub link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,9 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <footer>
-      <p>Designed by The Frontend Freestylers 2023 </p>
       <p>
-        <a id="GitHubLink" href="https://github.com/Nwinch1512/14-miix">
-          <FontAwesomeIcon icon={faGithub} /> Nwinch1512/14-miix
+        <a id="GitHubLink" href="https://github.com/RGBrain/miix">
+          <FontAwesomeIcon icon={faGithub} /> RGBrain/miix
         </a>
       </p>
     </footer>
